335e3ea88f5ee5fc0ebbdab4a15d742e9b47b2a9
kolljohnson <kjohnson@pillartechnology.com>
2018-08-02 11:55:08 -0500

    Initial Commit

---
+++ swiftkata/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftkata
+//
+//  Created by Koll Johnson on 8/2/18.
+//  Copyright © 2018 Pillar Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
